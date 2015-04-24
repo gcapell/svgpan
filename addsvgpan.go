007faa3e58604d46a74e2b49c0bab4ac4a323764
Gary Capell <gary.capell@gmail.com>
2015-04-24 09:39:13 -0700

    Added flags for in/out filename, script to inject

--- addsvgpan.go
+++ addsvgpan.go
@@ -9,7 +9,21 @@ import (
 	"log"
 	"os"
 	"strings"
+	"flag"
 )
+
+var (
+	script = flag.String("script", "https://raw.githubusercontent.com/gcapell/svgpan/master/SVGPan.js", "URL/file for SVGPan script")
+	outFile = flag.String("o", "", "filename to output to, defaults to stdout")
+	inFile = flag.String("i", "", "filename to read from, defaults to stdin")
+)
+
+func scanWhatever(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if len(data) == 0 {
+		return 0, nil, io.EOF
+	}
+	return len(data), data, nil
+}
 
 func scanXMLToken(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if len(data) == 0 {
@@ -38,13 +52,16 @@ func scanXMLToken(data []byte, atEOF bool) (advance int, token []byte, err error
 }
 
 // change/add id, prepend script
-func addPan(t string) string {
+func addPan(t string) (string, error) {
 	d := xml.NewDecoder(strings.NewReader(t))
 	tok, err := d.RawToken()
 	if err != nil {
-		log.Fatal("addPan", err)
+		return "", fmt.Errorf("%s decoding %q", err, tok)
 	}
-	s := tok.(xml.StartElement)
+	s, ok := tok.(xml.StartElement)
+	if !ok  {
+		return "", fmt.Errorf("expected %#v to be StartElement", tok)
+	}
 	for pos, a := range s.Attr {
 		if a.Name.Local == "id" {
 			a.Value = "viewport"
@@ -55,25 +72,28 @@ func addPan(t string) string {
 	var buf bytes.Buffer
 	enc := xml.NewEncoder(&buf)
 	if err := enc.EncodeToken(tok); err != nil {
-		log.Fatal("encodeToken", err)
+		return "", fmt.Errorf("%s encoding %#v", tok)
 	}
 	enc.Flush()
 
-	script := `<script xlink:href="http://svgpan.googlecode.com/svn/trunk/SVGPan.js"/>`
-	return script + "\n" + buf.String()
+	script := fmt.Sprintf(`<script xlink:href="%s"/>`, *script)
+	return script + "\n" + buf.String(), nil
 }
 
 func filterPan(src io.Reader, dst io.Writer) error {
 	s := bufio.NewScanner(src)
 	s.Split(scanXMLToken)
-	first := true
+	seeking := true	// ... for first g element
 	for s.Scan() {
 		tok := s.Text()
-		if first {
-			if strings.HasPrefix(tok, "<g") {
-				first = false
-				tok = addPan(tok)
+		if seeking && strings.HasPrefix(tok, "<g") {
+			seeking = false
+			var err error
+			tok, err = addPan(tok)
+			if err != nil {
+				return err
 			}
+			s.Split(scanWhatever)
 		}
 		if _, err := io.WriteString(dst, tok); err != nil {
 			return err
@@ -83,5 +103,23 @@ func filterPan(src io.Reader, dst io.Writer) error {
 }
 
 func main() {
-	filterPan(os.Stdin, os.Stdout)
+	flag.Parse()
+	inf := os.Stdin
+	var err error
+	if *inFile != "" {
+		inf, err = os.Open(*inFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	outf := os.Stdout
+	if *outFile != "" {
+		outf, err = os.Create(*outFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := filterPan(inf, outf); err != nil {
+		log.Fatal(err)
+	}
 }
